d998bdbe2678600ae1ed6fca23ac10028161a6c1
themastercut <hello@themastercut.co>
2018-06-28 13:27:44 +0200

    Refactor: Performance.

--- src/Components/PageTemplates.php
+++ src/Components/PageTemplates.php
@@ -12,7 +12,10 @@ use shellpress\v1_2_5\src\Shared\Components\IComponent;
 class PageTemplates extends IComponent {
 
 	/** @var string */
-	public $templatesDir;
+	protected $templatesDir;
+
+	/** @var string[] - Key: file name; Value: description; */
+	protected $supportedTemplates;
 
 	/**
 	 * Called on creation of component.
@@ -27,6 +30,10 @@ class PageTemplates extends IComponent {
 
 		$this->templatesDir = $this::s()->getPath( 'src/WpPageTemplates' );
 
+		$this->supportedTemplates = array(
+			'tmc-builder-custom.php'    =>  __( 'TMC Builder Custom', 'tmc_builder' )
+		);
+
 		//  ----------------------------------------
 		//  Filters
 		//  ----------------------------------------
@@ -37,35 +44,71 @@ class PageTemplates extends IComponent {
 
 	}
 
+	/**
+	 * Returns absolute path to templates directory.
+	 *
+	 * @return string
+	 */
+	public function getTemplatesDir() {
+
+		return $this->templatesDir;
+
+	}
+
+	/**
+	 * Returns templates names added by this plugin.
+	 *
+	 * @return string[]
+	 */
+	public function getSupportedTemplates() {
+
+		return $this->supportedTemplates;
+
+	}
+
 	//  ================================================================================
 	//  FILTERS
 	//  ================================================================================
 
 	/**
+	 * Inserts new templates for use in page template chooser.
+	 * Called on theme_page_templates.
+	 *
 	 * @param array $templates - Keys: filenames. Values: names.
 	 *
 	 * @return array
 	 */
 	public function _f_addCustomPageTemplates( $templates ) {
 
-		$templates['tmc-builder-custom.php'] = __( 'TMC Builder Custom', 'tmc_builder' );
-
-		return $templates;
+		return array_replace( $templates, $this->getSupportedTemplates() );
 
 	}
 
 	/**
+	 * Replaces default template file path with the one inside plugin directory.
+	 * Called on template_include.
+	 *
 	 * @param string $templatePath
 	 *
 	 * @return string
 	 */
 	public function _f_replaceTemplatePath( $templatePath ) {
 
-		//  Our custom templates are inside plugin directory.
-		//  It might not be one we are talking about so check if this file exists.
-		$newTemplatePath = trailingslashit( $this->templatesDir ) . get_page_template_slug();
+		$chosenPageTemplate = get_page_template_slug(); //  Page template chosen by user in editor.
 
-		return ( file_exists( $newTemplatePath ) ) ? $newTemplatePath : $templatePath;
+		//  TODO - Add possibility to overwrite template inside theme.
+
+		if( array_key_exists( $chosenPageTemplate, $this->getSupportedTemplates() ) ){
+
+			//  Our custom templates are inside plugin directory.
+			//  It might not be one we are talking about so check if this file exists.
+			$newTemplatePath = trailingslashit( $this->getTemplatesDir() ) . $chosenPageTemplate;
+
+			if( file_exists( $newTemplatePath ) ) return $newTemplatePath;
+
+		}
+
+		return $templatePath;
 
 	}
 
