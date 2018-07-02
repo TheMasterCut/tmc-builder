6f23d638c1aea338b3857941697feada7c243662
themastercut <hello@themastercut.co>
2018-07-02 15:56:43 +0200

    Add: On hover event stub.

--- assets/js/customizer.js
+++ assets/js/customizer.js
@@ -1,7 +1,32 @@
 jQuery( document ).ready( function( $ ) {
 
+    //  ----------------------------------------
+    //  Enable sorting of widgets
+    //  ----------------------------------------
+
     $( '.tmc-builder-widgets-area' ).sortable( {
         'items' :   '> .widget'
+    } );
+
+    //  ----------------------------------------
+    //  Nice hover effect
+    //  ----------------------------------------
+
+    wp.customize.bind( 'preview-ready', function() {
+
+        $( '.tmc-builder-widgets-area .customize-partial-edit-shortcut' ).hover(
+            function() {
+
+                $( this ).closest( '.widget' ).addClass( 'hover' );
+
+            },
+            function() {
+
+                $( this ).closest( '.widget' ).removeClass( 'hover' );
+
+            }
+        );
+
     } );
 
 } );
